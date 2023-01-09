dfc4e9f56c425e05b727e1d8d10d3af274c430f0
Jason Stratman <jason@stratmanmedia.com>
2023-01-09 13:19:07 -0600

    Refactored cadence components and pages

--- src/omnibudget-ng/src/app/core/transactions/cadence-model.ts
+++ src/omnibudget-ng/src/app/core/transactions/cadence-model.ts
@@ -1,3 +1,5 @@
+import { AccountModel } from "../accounts/account-model";
+import { CategoryModel } from "../categories/category-model";
 import { TimePeriod } from "../models/time-period-enum";
 
 export interface CadenceModel {
@@ -5,7 +7,9 @@ export interface CadenceModel {
   name: string;
   description: string;
   accountGuid: string;
+  account: AccountModel;
   categoryGuid: string;
+  category: CategoryModel;
   amount: number;
   interval: number;
   timePeriod: TimePeriod;
--- src/omnibudget-ng/src/app/modules/cadences/pages/cadence-main/cadence-main.component.ts
+++ src/omnibudget-ng/src/app/modules/cadences/pages/cadence-main/cadence-main.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { combineLatest, map, Observable } from 'rxjs';
+import { combineLatest, map, Observable, Subject } from 'rxjs';
 import { AccountService } from 'src/app/core/accounts/account.service';
 import { CategoryService } from 'src/app/core/categories/category.service';
 import { LoggingService } from 'src/app/core/logging/logging.service';
 import { CadenceModel } from 'src/app/core/transactions/cadence-model';
-import { CadencesService } from 'src/app/core/transactions/cadences.service';
+import { CadenceService } from 'src/app/core/transactions/cadence.service';
 
 @Component({
   selector: 'omni-cadence-main',
@@ -12,31 +12,33 @@ import { CadencesService } from 'src/app/core/transactions/cadences.service';
   styleUrls: ['./cadence-main.component.css']
 })
 export class CadenceMainComponent implements OnInit {
-  private _logger = new LoggingService('CadenceMainComponent');
+  private _logger: LoggingService = new LoggingService({
+    callerName: "CadenceMainComponent"
+  });
+  private ngDestroy$: Subject<boolean>;
+
   cadenceList: Observable<CadenceModel[]>;
 
   constructor(
-    private _cadenceService: CadencesService,
-    private _accountServie: AccountService,
+    private _cadenceService: CadenceService,
+    private _accountService: AccountService,
     private _categoryService: CategoryService) { }
 
   ngOnInit(): void {
-    this.cadenceList = this._cadenceService.getAll().pipe(map(
-      cadences => {
-        cadences.map(c => {
-          combineLatest([
-            this._accountServie.getOne(c.accountGuid),
-            this._categoryService.getOne(c.categoryGuid)
-          ]).subscribe(
-            ([account, category]) => {
-              c.account = account || undefined;
-              c.category = category;
-            }
-          );
-        });
+    this.cadenceList = combineLatest([
+      this._cadenceService.getAll(),
+      this._accountService.getAll(),
+      this._categoryService.getAll()
+    ]).pipe(
+      map(([cadences, accounts, categories]) => {
+        cadences.map(cadence => ({
+          ...cadence,
+          account: accounts.find(acc => acc.guid === cadence.accountGuid),
+          category: categories.find(cat => cat.guid === cadence.categoryGuid)
+        } as CadenceModel));
         return cadences;
-      }
-    ));
+      })
+    );
   }
 
   editCadence(guid: string): void {
