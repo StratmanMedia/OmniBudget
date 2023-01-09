ea0f5f865a651d02bfe3a203ad7c35051d80d0d2
Jason Stratman <jason@stratmanmedia.com>
2023-01-08 21:09:14 -0600

    Improvements to storage service

--- src/omnibudget-ng/src/app/core/storage/storage.service.ts
+++ src/omnibudget-ng/src/app/core/storage/storage.service.ts
@@ -6,35 +6,40 @@ import { LoggingService } from '../logging/logging.service';
   providedIn: 'root'
 })
 export class StorageService {
-
-  private _logger: LoggingService = new LoggingService('StorageService');
+  private _logger: LoggingService = new LoggingService({
+    callerName: 'StorageService'
+  });
   private _store: Storage = localStorage;
 
   constructor() { }
 
   create(key: string, data: any): Observable<void> {
+    this._logger.debug(`create ENTER.`);
     return new Observable((o: Observer<void>) => {
       const json = JSON.stringify(data);
+      this._logger.debug(`Adding to local storage. key=${key}, data=${json}`);
       this._store.setItem(key, json);
       o.next();
       o.complete();
     });    
   }
 
-  read<T>(key: string): Observable<T> {
-    return new Observable((o: Observer<T>) => {
+  read<T>(key: string): Observable<T | null> {
+    this._logger.debug(`read ENTER.`);
+    return new Observable((o: Observer<T | null>) => {
       const json = this._store.getItem(key);
-      const data = (json !== null) ? <T>JSON.parse(json) : <T>JSON.parse('[]');
+      const data = (!!json) ? JSON.parse(json) as T : null;
+      this._logger.debug(`Reading from local storage. key=${key}, data=${json}`);
       o.next(data);
       o.complete();
     });
   }
 
   update(key: string, data: any): Observable<void> {
-    this._logger.debug(`StorageService: Entering update.`);
+    this._logger.debug(`update ENTER.`);
     return new Observable((o: Observer<void>) => {
       const json = JSON.stringify(data);
-      this._logger.debug(`StorageService: Updating local storage. Key=${key}, Data=${JSON.stringify(data)}`);
+      this._logger.debug(`Updating local storage. Key=${key}, Data=${json}`);
       this._store.setItem(key, json);
       o.next();
       o.complete();
@@ -42,7 +47,9 @@ export class StorageService {
   }
 
   delete(key: string): Observable<void> {
+    this._logger.debug(`delete ENTER.`);
     return new Observable((o: Observer<void>) => {
+      this._logger.debug(`Removing from local storage. key=${key}`);
       this._store.removeItem(key);
       o.next();
       o.complete();
