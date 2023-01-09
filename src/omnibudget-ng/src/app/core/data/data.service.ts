a36831749f2f73faed1adc30c35640c5bd42da52
Jason Stratman <jason@stratmanmedia.com>
2023-01-09 07:34:38 -0600

    Improvements to data service

--- src/omnibudget-ng/src/app/core/data/data.service.ts
+++ src/omnibudget-ng/src/app/core/data/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, concatMap, first, map, Observable, Subject, takeUntil } from 'rxjs';
+import { BehaviorSubject, combineLatest, concatMap, first, map, Observable, of, Subject, takeUntil } from 'rxjs';
 import { LoggingService } from '../logging/logging.service';
 import { StorageService } from '../storage/storage.service';
 
@@ -8,13 +8,13 @@ import { StorageService } from '../storage/storage.service';
 })
 export abstract class DataService<T> {
   private ngDestroy$ = new Subject<boolean>();
-  protected _dataSubject$ = new BehaviorSubject<T[]>([]);
+  protected _data$ = new BehaviorSubject<T[]>([]);
   
   constructor(
     private _storage: StorageService) {
 
     this.loadFromStorage();
-    this._dataSubject$.pipe(
+    this._data$.pipe(
       takeUntil(this.ngDestroy$),
       concatMap((data: T[]) => {
         this.logger().debug(`New data emitted. data=${JSON.stringify(data)}`);
@@ -25,23 +25,23 @@ export abstract class DataService<T> {
 
   add(newData: T): Observable<void> {
     this.logger().debug(`Adding data. data=${JSON.stringify(newData)}`);
-    return this._dataSubject$.pipe(
+    return this._data$.pipe(
       first(),
       map(data => {
         this.modifyDataBeforeAdd(newData);
         data.push(newData);
-        this._dataSubject$.next(data);
+        this._data$.next(data);
       })
     );
   }
     
   getAll(): Observable<T[]> {
-    return this._dataSubject$.asObservable();
+    return this._data$.asObservable();
   }
 
-  getOne(id: string | number): Observable<T | null> {
-    return this._dataSubject$.pipe(
-      map(data => {
+  getOne(id: string | number): Observable<T | undefined> {
+    return this._data$.pipe(
+      concatMap(data => {
         return this.findExistingItem(id);
       })
     );
@@ -49,25 +49,31 @@ export abstract class DataService<T> {
 
   update(id: string | number, updatedData: T): Observable<void> {
     this.logger().debug(`Updating data=${JSON.stringify(updatedData)}`);
-    return this._dataSubject$.pipe(
+    return this._data$.pipe(
       first(),
-      map(data => {
-        let existing = this.findExistingItem(id);
+      concatMap(data => {
+        const existing = this.findExistingItem(id);
+        return combineLatest([of(data), existing]);
+      }),
+      map(([data, existing]) => {
         if (!existing) { return; }
         this.updateData(existing, updatedData);
-        this._dataSubject$.next(data);
+        this._data$.next(data);
       })
     );
   }
 
   delete(id: string | number): Observable<void> {
     this.logger().debug(`Deleting data. id=${id}`);
-    return this._dataSubject$.pipe(
+    return this._data$.pipe(
       first(),
-      map(data => {
+      concatMap(data => {
         const index = this.findIndex(id);
+        return combineLatest([of(data), index]);
+      }),
+      map(([data, index]) => {
         data.splice(index, 1);
-        this._dataSubject$.next(data);
+        this._data$.next(data);
       })
     );
   }
@@ -79,10 +85,10 @@ export abstract class DataService<T> {
       map(data => {
         this.logger().debug(`Local storage=${JSON.stringify(data)}`);
         if (!data) {
-          this._dataSubject$.next([]);
+          this._data$.next([]);
           return;
         }
-        this._dataSubject$.next(data);
+        this._data$.next(data);
       })
     );
   }
@@ -90,7 +96,7 @@ export abstract class DataService<T> {
   abstract logger(): LoggingService;
   abstract localStorageKey(): string;
   abstract modifyDataBeforeAdd(data: T): void;
-  abstract findExistingItem(id: string | number): T | null;
+  abstract findExistingItem(id: string | number): Observable<T | undefined>;
   abstract updateData(current: T, updated: T): void;
-  abstract findIndex(id: string | number): number;
+  abstract findIndex(id: string | number): Observable<number>;
 }
