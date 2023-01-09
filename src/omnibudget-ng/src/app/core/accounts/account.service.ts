3f89229c37e0b34a0f1a943a93ef1b9a01a6f12c
Jason Stratman <jason@stratmanmedia.com>
2023-01-09 07:35:21 -0600

    Account service now extends data service

--- src/omnibudget-ng/src/app/core/accounts/account.service.ts
+++ src/omnibudget-ng/src/app/core/accounts/account.service.ts
@@ -1,91 +1,48 @@
 import { Injectable } from '@angular/core';
-import { Observable, Observer, ReplaySubject } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Guid } from 'src/app/shared/classes/guid';
 import { LocalStorageKeys } from 'src/app/shared/classes/local-storage-keys';
+import { DataService } from '../data/data.service';
 import { LoggingService } from '../logging/logging.service';
-import { StorageService } from '../storage/storage.service';
 import { AccountModel } from './account-model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AccountService {
+export class AccountService extends DataService<AccountModel> {
+  private _logger: LoggingService = new LoggingService({
+    callerName: "AccountService"
+  });
 
-  private _logger: LoggingService = new LoggingService("AccountService");
-  private _accountStore: AccountModel[] = [];
-  private _accountStoreSubject = new ReplaySubject<AccountModel[]>(1);
-
-  constructor(
-    private _storage: StorageService) {
-      this.refreshStore();
-      this._accountStoreSubject.subscribe(
-        (accounts: AccountModel[]) => {
-          this._logger.debug('updating local storage.');
-          this._storage.update(LocalStorageKeys.accountStore, accounts).subscribe();
-        });
-    }
-
-  add(account: AccountModel): Observable<void> {
-    this._logger.debug(`adding account=${JSON.stringify(account)}`);
-    return new Observable((o: Observer<void>) => {
-      account.guid = Guid.newGuid().toString();
-      this._accountStore.push(account);
-      this._accountStoreSubject.next(this._accountStore);
-      o.next();
-      o.complete();
-    });
+  logger(): LoggingService {
+    return this._logger;
   }
 
-  getAll(): Observable<AccountModel[]> {
-    this._logger.debug('getAll.');
-    return this._accountStoreSubject.asObservable();
+  localStorageKey(): string {
+    return LocalStorageKeys.accountStore;
   }
 
-  getOne(guid: string): Observable<AccountModel> {
-    this._logger.debug(`getOne. guid=${guid}`);
-    return new Observable((o: Observer<AccountModel>) => {
-      const account = this._accountStore.find(a => a.guid === guid);
-      if (account) {
-        o.next(account);
-      }
-      o.complete();
-    });
+  modifyDataBeforeAdd(data: AccountModel): void {
+    data.guid = Guid.newGuid.toString();
   }
 
-  update(account: AccountModel): Observable<void> {
-    this._logger.debug(`update. account=${JSON.stringify(account)}`);
-    return new Observable((o: Observer<void>) => {
-      let existing = this._accountStore.find(a => a.guid === account.guid);
-      if (existing == null || existing == undefined) {
-        o.error('Account does not exist.');
-        o.complete();
-        return;
-      }
-      existing.name = account.name;
-      this._accountStoreSubject.next(this._accountStore);
-      o.next();
-      o.complete();
-    });
+  findExistingItem(id: string | number): Observable<AccountModel | undefined> {
+    return this._data$.pipe(
+      map(accounts => {
+        return accounts.find(a => a.guid === id);
+      })
+    );
   }
 
-  delete(guid: string): Observable<void> {
-    this._logger.debug(`Delete account. guid=${guid}`);
-    return new Observable((o: Observer<void>) => {
-      const index = this._accountStore.findIndex(a => a.guid === guid);
-      this._accountStore.splice(index, 1);
-      this._accountStoreSubject.next(this._accountStore);
-      o.next();
-      o.complete();
-    });
+  updateData(current: AccountModel, updated: AccountModel): void {
+    current.name = updated.name;
   }
 
-  private refreshStore(): void {
-    this._logger.debug('account store refresh.');
-    this._storage.read<AccountModel[]>(LocalStorageKeys.accountStore).subscribe(
-      (accounts: AccountModel[]) => {
-        this._logger.debug(`account store=${JSON.stringify(accounts)}`);
-        this._accountStore = accounts;
-        this._accountStoreSubject.next(accounts);
-      });
+  findIndex(id: string | number): Observable<number> {
+    return this._data$.pipe(
+      map(accounts => {
+        return accounts.findIndex(a => a.guid === id);
+      })
+    );
   }
 }
